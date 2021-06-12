c4e0d22987967b0c2486fc5237f8040a7c017269
Danilo Pereira <danilo4web@gmail.com>
2021-06-12 18:49:43 -0300

    fix draw method on SavingAccount class

--- src/FactoryPattern/SavingAccount.php
+++ src/FactoryPattern/SavingAccount.php
@@ -19,7 +19,7 @@ class SavingAccount extends AccountAbstract implements SavingAccountInterface
     {
         $balance = $this->getBalance();
         if ($value > $balance) {
-            throw new Exception ("Balance isn't enough, you have only: $this->balance. Can't withdraw: " . $value);
+            throw new Exception ("Balance isn't enough, you have only: $balance. Can't withdraw: " . $value);
         }
 
         $newBalance = $balance - $value;
